d90b0a7d6b1f0f3979cd55885d36801047c3a416
Alan <alan@tuillo.com>
2015-05-07 17:05:44 +0200

    Initial commit

---
+++ app/src/main/java/com/geoalgorithm/googlelocationtestapp/LocationService.java
@@ -0,0 +1,136 @@
+package com.geoalgorithm.googlelocationtestapp;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Alan Poggetti on 05/05/2015.
+ */
+public class LocationService extends Service implements GoogleApiClient.ConnectionCallbacks, LocationListener, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        // code to execute when the service is first created
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startid) {
+        buildGoogleApiClient();
+
+        mGoogleApiClient.connect();
+
+        return START_STICKY;
+
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        Log.e("Google API", "Connection failed... Error: "+connectionResult);
+
+    }
+
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+        Log.d("Google API", "Suspended...");
+
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1*60*1000);
+        mLocationRequest.setSmallestDisplacement(15);
+        mLocationRequest.setFastestInterval(1*60*1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+        Log.d("Google API", "Connected...");
+
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        List<Location> mLocations = new ArrayList<>();
+
+        SharedPreferences prefs = getSharedPreferences("GooglePlayMapsTest", Context.MODE_PRIVATE);
+        String locationJson = prefs.getString("locationJson", "");
+
+        if (!locationJson.equals("")) {
+
+            Gson gson = new Gson();
+            mLocations = gson.fromJson(locationJson, new TypeToken<ArrayList<Location>>() {}.getType());
+
+        }
+
+        mLocations.add(location);
+
+        Gson gson = new GsonBuilder().create();
+
+        locationJson = gson.toJson(mLocations, new TypeToken<List<Location>>() {
+        }.getType());
+
+        SharedPreferences.Editor editor = getSharedPreferences("GooglePlayMapsTest", Context.MODE_PRIVATE).edit();
+
+        editor.putString("locationJson", locationJson).commit();
+
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(new Intent(MapsActivity.NEW_LOCATION_RECEIVED));
+
+    }
+
+    protected void startLocationUpdates() {
+
+        if(mLocationRequest == null)
+            createLocationRequest();
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+}
---
+++ app/src/main/java/com/geoalgorithm/googlelocationtestapp/MapsActivity.java
@@ -0,0 +1,257 @@
+package com.geoalgorithm.googlelocationtestapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.location.Location;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private List<Location> mLocations;
+    private List<Marker> mMarkers = new ArrayList<>();
+    private Marker lastMarker;
+
+    public static final String NEW_LOCATION_RECEIVED = "new_location_received";
+
+    private List<String> mLocationTitles = new ArrayList<>();
+    private ListView mDrawerList;
+    private MyArrayAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_maps);
+
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        adapter = new MyArrayAdapter(this, mLocationTitles,
+                R.layout.list_item);
+        mDrawerList.setAdapter(adapter);
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        setUpMapIfNeeded();
+
+        //We start the location service
+        startService(new Intent(this, LocationService.class));
+
+    }
+
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps between markers */
+    private void selectItem(int position) {
+
+        mDrawerList.setItemChecked(position, true);
+        moveCamera(mLocations.get(position));
+
+        if(lastMarker != null)
+            lastMarker.hideInfoWindow();
+
+        lastMarker = mMarkers.get(position);
+
+        lastMarker.showInfoWindow();
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        setUpMapIfNeeded();
+
+        //We register our location receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(mLocationReceiver, new IntentFilter(NEW_LOCATION_RECEIVED));
+
+    }
+
+    /**
+     * Let's not forget to unregister our Receiver and stop location updates
+     */
+    @Override
+    protected void onStop(){
+
+        super.onStop();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mLocationReceiver);
+
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated..
+     *
+     * Also, we initialize the Library with our provided API Key
+     */
+
+    private void setUpMapIfNeeded() {
+
+        if (mMap == null) {
+
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+
+            if(mLocations == null && mMap != null) {
+
+                loadMarkersOnList(true,false);
+
+            }
+
+        }
+
+    }
+
+    private void loadMarkersOnList(boolean loadMarkersOnMap, boolean loadLastMarker){
+
+        SharedPreferences prefs = getSharedPreferences("GooglePlayMapsTest", Context.MODE_PRIVATE);
+        String locationJson = prefs.getString("locationJson", "");
+
+        if(!locationJson.equals("")){
+
+            Gson gson = new Gson();
+            mLocations = gson.fromJson(locationJson,new TypeToken<ArrayList<Location>>(){}.getType());
+
+            Collections.reverse(mLocations);
+
+        }
+        if(mLocations == null){
+            mLocations = new ArrayList<>();
+        }
+
+        if(mLocationTitles == null)
+            mLocationTitles = new ArrayList<>();
+        else
+            mLocationTitles.clear();
+
+        for(Location location : mLocations) {
+
+            String date = Utils.dateFormatter(new Date(location.getTime()));
+
+            if(loadMarkersOnMap) {
+
+                addMarker(location,false);
+
+            }
+            mLocationTitles.add(date + ", Accuracy = " + location.getAccuracy() + " m");
+
+        }
+
+        if(loadLastMarker && mLocations.size()>0){
+            addMarker(mLocations.get(0),true);
+        }
+
+        if(mLocations.size()>0)
+         selectItem(0);
+
+       adapter = new MyArrayAdapter(this,mLocationTitles,
+                R.layout.list_item);
+
+        mDrawerList.setAdapter(adapter);
+
+    }
+
+
+
+
+
+
+    /**
+     * We configure our BroadcastReceiver in order to store our new location and place a marker on our map
+     */
+    private final BroadcastReceiver mLocationReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if(intent.getAction().equals(NEW_LOCATION_RECEIVED)){
+
+                //We make sure that we have a valid location
+                if(mMap != null){
+
+                    SharedPreferences prefs = getSharedPreferences("GooglePlayMapsTest", Context.MODE_PRIVATE);
+                    String locationJson = prefs.getString("locationJson", "");
+
+                    if(!locationJson.equals("")){
+
+                        loadMarkersOnList(false, true);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    };
+
+    /**
+     * Simple method that will move the camera to a location in our map
+     * @param location
+     */
+
+    private void moveCamera(Location location){
+
+        CameraUpdate zoom=CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),
+                location.getLongitude()), 15);
+
+        mMap.animateCamera(zoom);
+
+    }
+
+
+    /**
+     * Simple method that will add a marker on our map and marker collection with a snippet
+     * @param location
+     */
+    private void addMarker(Location location,boolean setFirst){
+
+        Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),
+                location.getLongitude())).title("Location with accuracy " + location.getAccuracy()));
+
+        String date = Utils.dateFormatter(new Date(location.getTime()));
+
+        marker.setSnippet("Taken "+date);
+
+        if(setFirst)
+            mMarkers.add(0,marker);
+        else
+            mMarkers.add(marker);
+
+    }
+
+
+}
